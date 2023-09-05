b453d58621356fa8c766ad474c3832d993959565
dpc-profile <daniel.pcastro11@gmail.com>
2023-09-05 15:44:26 -0300

    init

---
+++ Srv/Api/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
