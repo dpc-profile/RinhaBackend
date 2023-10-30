b24915d3036a93875cc040dd1b10a6b03d998817
Daniel Pereira <daniel.pcastro11@gmail.com>
2023-10-30 21:01:14 +0000

    testes do GetPorUuidAsync

--- Srv/Controllers/UsuarioController.cs
+++ Srv/Controllers/UsuarioController.cs
@@ -64,6 +64,4 @@ public class UsuarioController : ControllerBase
     {
         return await _usuarioServices.CountUsuariosCadastradosAsync();
     }
-
-    
 }
--- Tests/tests/ValidaRetornosAPITest.cs
+++ Tests/tests/ValidaRetornosAPITest.cs
@@ -4,7 +4,6 @@ using Api.Controllers;
 using Api.Services;
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Any;
 
 using Moq;
 
@@ -78,5 +77,55 @@ public class ValidaRetornosAPITest
         // Assert
         Assert.NotNull(result);
         Assert.Equal(expected: (int)HttpStatusCode.UnprocessableEntity, actual: result.StatusCode);
+    }
+
+    [Fact]
+    public async void RetornarOkVazio_AoConsultarUUIDInexistente()
+    {
+        // Arrange
+        var mockRepo = new Mock<IUsuarioServices>();
+        var controller = new UsuarioController(mockRepo.Object);
+
+        // Act
+        var result = await controller.GetPorUuidAsync("uuidQueNaoExiste");
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<string>>(result);
+        Assert.NotNull(actionResult);
+
+        var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        Assert.Equal(expected: (int)HttpStatusCode.OK, actual: okObjectResult.StatusCode);
+        Assert.Null(okObjectResult.Value);
+    }
+
+    [Fact]
+    public async void RetornarOkComUsuario_AoConsultarUUIDExistente()
+    {
+        string uuid = "780a47a3-9ef5-4d97-ba9e-b17bab0f2f35";
+        UsuarioModel usuario = new ()
+        {
+            Id = uuid,
+            Nome = "Teste1",
+            Nascimento = "2023-11-23",
+            Apelido = "XxTest360xX",
+            Stack = "C#, Java, NodeJS",
+            
+        };
+        var mockRepo = new Mock<IUsuarioServices>();
+        mockRepo.Setup(x => x.ConsultaPorUUIDAsync(It.IsAny<string>())).ReturnsAsync(usuario);
+
+        var controller = new UsuarioController(mockRepo.Object);
+
+        var result = await controller.GetPorUuidAsync(uuid);
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<string>>(result);
+        Assert.NotNull(actionResult);
+
+        var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        Assert.Equal(expected: (int)HttpStatusCode.OK, actual: okObjectResult.StatusCode);
+
+        var returnValue = Assert.IsType<UsuarioModel>(okObjectResult.Value);
+        Assert.NotNull(returnValue);
     }
 }
